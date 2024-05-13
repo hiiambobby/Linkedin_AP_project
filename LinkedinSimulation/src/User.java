6cca6ed0efbab99b9535967c3f6a230d7dd0ce7e
hiiiambobby <saba3st@gmail.com>
2024-05-13 14:40:33 +0330

    user created

---
+++ LinkedinSimulation/src/User.java
@@ -0,0 +1,29 @@
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
