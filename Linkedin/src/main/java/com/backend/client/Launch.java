22a3a65dce438dc8b553039fa351180b14d683d3
hiiiambobby <saba3st@gmail.com>
2024-06-24 03:18:54 +0330

    more edits on sign in/up page,still it is wrong

--- Linkedin/src/main/java/com/backend/client/Launch.java
+++ Linkedin/src/main/java/com/backend/client/Launch.java
@@ -12,21 +12,40 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Objects;
 
+//
+//public class Launch extends Application {
+//
+//    @Override
+//    public void start(Stage stage) throws Exception {
+//        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Main.fxml")));
+//        Scene scene = new Scene(root);
+//        scene.setFill(Color.TRANSPARENT);
+//        stage.setScene(scene);
+//        stage.initStyle(StageStyle.TRANSPARENT);
+//        stage.show();
+//    }
 
-public class Launch extends Application {
+    public class Launch extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        Scene scene = new Scene(root);
-        scene.setFill(Color.TRANSPARENT);
-        stage.setScene(scene);
-        stage.initStyle(StageStyle.TRANSPARENT);
-        stage.show();
-    }
+        @Override
+        public void start(Stage primaryStage) throws Exception {
+            FXMLLoader loader = new FXMLLoader();
+            final String address = "/com/backend/client/Main.fxml";
+            final InputStream fxmlStream = getClass().getResourceAsStream(address);
 
+            if (fxmlStream == null) {
+                throw new FileNotFoundException("FXML file not found: " + address);
+            }
+
+            final Parent root = loader.load(fxmlStream);
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
 
     public static void main(String[] args) {
         launch(args);
