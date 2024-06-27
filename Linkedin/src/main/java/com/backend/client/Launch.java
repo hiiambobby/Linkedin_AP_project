506ec9193f70468d1d1c9a0a22aa858afab3d06f
hiiiambobby <saba3st@gmail.com>
2024-06-27 17:56:08 +0330

    icon and title added

--- Linkedin/src/main/java/com/backend/client/Launch.java
+++ Linkedin/src/main/java/com/backend/client/Launch.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -16,6 +17,9 @@ import javafx.stage.StageStyle;
         @Override
         public void start(Stage stage) throws Exception {
             Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
+            stage.setTitle("Mewedin");
+            Image icon = new Image("/img/photo_2024-05-15_16-05-20.jpg");
+            stage.getIcons().add(icon);
             Scene scene = new Scene(root);
             scene.setFill(Color.TRANSPARENT);
             stage.setScene(scene);
--- Linkedin/src/main/java/com/backend/client/controllers/Try.java
+++
@@ -1,4 +0,0 @@
-package com.backend.client.controllers;
-
-public class Try {
-}
