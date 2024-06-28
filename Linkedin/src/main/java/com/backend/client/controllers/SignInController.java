6c5a98cee9dd5bb91e67b67beabf8b982ebdf0fa
hiiiambobby <saba3st@gmail.com>
2024-06-28 17:21:15 +0330

    sigin logic completed. to do: handle errors better

--- Linkedin/src/main/java/com/backend/client/controllers/SignInController.java
+++ Linkedin/src/main/java/com/backend/client/controllers/SignInController.java
@@ -5,6 +5,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 public class SignInController {
     @FXML
@@ -16,8 +26,7 @@ public class SignInController {
     @FXML
     private Label msgId;
 
-    public void handleSignIn(ActionEvent event)
-    {
+    public void handleSignIn(ActionEvent event) throws IOException {
        String email = emailField.getText();
        String pass = passField.getText();
        if(email.length() == 0||pass.length() == 0)
@@ -30,7 +39,29 @@ public class SignInController {
 
     }
 
-    private void sendRequest(String email, String pass) {
-        System.out.println("hala vaght hast.....");
+    private void sendRequest(String email, String pass) throws IOException {
+        //logics to send request to http
+        URL url = new URL("http://localhost:8000/login"); // Replace with your server URL
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json; utf-8");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setDoOutput(true);
+
+        JSONObject jsonInput = new JSONObject();
+
+        jsonInput.put("email", email);
+        jsonInput.put("password", pass);
+
+
+        String jsonInputString = jsonInput.toString();
+
+        try (OutputStream os = conn.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        }
+        int responseCode = conn.getResponseCode();
+        System.out.println("POST Response Code :: " + responseCode);
+
     }
 }
