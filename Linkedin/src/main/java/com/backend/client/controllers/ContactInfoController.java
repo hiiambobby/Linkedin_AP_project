879e8a590a1438a9efbd165e6c8d5067af292f61
hiiiambobby <saba3st@gmail.com>
2024-06-29 20:50:11 +0330

    steps to make the contact info

---
+++ Linkedin/src/main/java/com/backend/client/controllers/ContactInfoController.java
@@ -0,0 +1,8 @@
+package com.backend.client.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ContactInfoController{
+
+}
--- Linkedin/src/main/java/com/backend/client/controllers/ProfileController.java
+++ Linkedin/src/main/java/com/backend/client/controllers/ProfileController.java
@@ -1,4 +1,28 @@
 package com.backend.client.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import java.io.IOException;
+
 public class ProfileController {
+
+    @FXML
+    private Button contactInfoBtn;
+
+    public void openContactInfo(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/ContactInfo.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+
+    }
+
+
 }
