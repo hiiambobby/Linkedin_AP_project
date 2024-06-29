eccc68ac609f5640fbf11e5b722d1ba72ec04bc1
hiiiambobby <saba3st@gmail.com>
2024-06-29 21:12:53 +0330

    save button added to contact inf//still need some work

--- Linkedin/src/main/java/com/backend/client/controllers/ContactInfoController.java
+++ Linkedin/src/main/java/com/backend/client/controllers/ContactInfoController.java
@@ -1,8 +1,27 @@
 package com.backend.client.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
 
 public class ContactInfoController{
+@FXML
+    private Button saveBtn;
+public void Save(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Profile.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
 
 }
