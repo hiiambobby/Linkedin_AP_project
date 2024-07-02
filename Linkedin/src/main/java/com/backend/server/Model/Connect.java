ff912a791b7b154ec9bb2423b20d3f7991d1125e
hiiiambobby <saba3st@gmail.com>
2024-07-03 01:43:51 +0330

    tool bar made in the search window

--- Linkedin/src/main/java/com/backend/server/Model/Connect.java
+++ Linkedin/src/main/java/com/backend/server/Model/Connect.java
@@ -3,14 +3,52 @@ package com.backend.server.Model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Connect {
-    @JsonProperty("senderName")
+    @JsonProperty("sender")
     private String senderName;
 
-    @JsonProperty("senderFamilyName")
-    private String familyName;
+    @JsonProperty("receiver")
+    private String receiverName;
 
+    @JsonProperty("notes")
+    private String notes;
+    @JsonProperty("accepted")
+    private boolean accepted;
+
+    public Connect(String senderName, String receiverName, String notes, boolean accepted) {
+        this.notes = notes;
+        this.senderName = senderName;
+        this.receiverName = receiverName;
+        this.accepted = accepted;
+    }
+
+    public void setAccepted(boolean accepted) {
+        this.accepted = accepted;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
 
 }
-
 
 
