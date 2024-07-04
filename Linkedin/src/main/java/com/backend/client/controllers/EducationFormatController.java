f4700c51a8f93689499600eb2ee679b8e63b3c36
MediyaAD <mediya.ey@gmail.com>
2024-07-04 04:32:27 +0330

    post and education fxml and their client controllers added

---
+++ Linkedin/src/main/java/com/backend/client/controllers/EducationFormatController.java
@@ -0,0 +1,35 @@
+package com.backend.client.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+public class EducationFormatController {
+    @FXML
+    private Label activityLabel;
+
+    @FXML
+    private Label degreeLabel;
+
+    @FXML
+    private Label descriptionLabel;
+
+    @FXML
+    private Label endMonthLabel;
+
+    @FXML
+    private Label endYearLabel;
+
+    @FXML
+    private Label schoolLabel;
+
+    @FXML
+    private Label skillsLabel;
+
+    @FXML
+    private Label startMonthLabel;
+
+    @FXML
+    private Label startYearLabel;
+
+    @FXML
+    private Label studyFieldLabel;
+}
---
+++ Linkedin/src/main/java/com/backend/client/controllers/PostController.java
@@ -0,0 +1,47 @@
+package com.backend.client.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class PostController {
+
+    @FXML
+    private Label captionLabel;
+
+    @FXML
+    private Label commentsCounterLabel;
+
+    @FXML
+    private Button likeBtn;
+
+    @FXML
+    private Label likesCounterLabel;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private ImageView postImage;
+
+    @FXML
+    private ImageView profilePhoto;
+
+    @FXML
+    void commentSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void likeSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewCommentsSelected(ActionEvent event) {
+
+    }
+
+}
